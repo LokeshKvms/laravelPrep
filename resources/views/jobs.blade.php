fb57263985a0f0e16a5c05cdb69caf57e9a0e27a
LokeshKvms <loki.kvms@gmail.com>
2025-05-28 14:36:09 +0530

    Ep 15

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,7 +3,7 @@
         Job Listings
     </x-slot:heading>
 
-    <div class="max-w-3xl mx-auto mt-6 px-4">
+    {{-- <div class="max-w-3xl mx-auto mt-6 px-4">
         <div class="overflow-x-auto rounded-lg shadow-md border border-gray-300">
             <table class="table-fixed min-w-full divide-y divide-gray-200 text-sm bg-white">
                 <thead class="bg-gray-900">
@@ -24,6 +24,22 @@
                     @endforeach
                 </tbody>
             </table>
+        </div>
+    </div> --}}
+
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}" class="block bg-white px-4 py-6 border border-gray-200 rounded-lg shadow">
+                <div class=" text-xl text-gray-900 font-bold">{{ $job->employer->name }}</div>
+                <strong class="text-gray-600">
+                    {{ $job['title'] }}:
+                </strong>
+                Pays {{ $job['salary'] }} per month.
+            </a>
+        @endforeach
+
+        <div>
+            {{ $jobs->links() }}
         </div>
     </div>
 </x-layout>
